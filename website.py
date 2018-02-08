1bcf75f73e2b913a644aa3f2ce80d6788acba33d
davidgroapa <15wgroapa@stjosephsrush.ie>
2018-02-08 15:18:30 +0000

    Create website.py

---
+++ website.py
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Coding at St.Josephs Rush</title>
+</head>
+<body>
+.header{width:100%;
+            position:fixed;
+            top:0px;
+            background: #00FFFF;
+            font-size: 30px;
+      text-align: center;}
+
+<h1>First time coding</h1>
+<p>3 years ago our first years were the first class to do coding.
+<img src="https://www.code4fun.com.au/wp-content/uploads/2014/09/Cover1.jpg"  style="width:400px;height:400px;">.
+It was a great year for all.Today we still teach coding for students that want to do it. We even have after school coding classes.
+Big companies like Google an Intel let our students in for a school trip.
+<img src="https://storage.googleapis.com/gd-wagtail-prod-assets/original_images/evolving_google_identity_videoposter_006.jpg"  style="width:400px;height:400px;">.
+
+
+</body>
+</html>
